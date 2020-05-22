997b46d75217c234f8f9ba5c6896434d4f772930
peter <peter@peter-TUXEDO>
2020-05-22 17:58:50 +0200

    initial + address-manager

---
+++ unit-tests/src/test/java/com/sap/cld200/ax/UnitTest.java
@@ -0,0 +1,12 @@
+package com.sap.cld200.ax;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
